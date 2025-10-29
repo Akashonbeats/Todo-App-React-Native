06b7422d345f6e7b9ab16f0023beb273a216e104
Akashonbeats <akash.s1204@gmail.com>
2025-10-29 09:57:48 +0530

    Implemented Dark/Light Modes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,22 @@
+import useTheme from "@/hooks/useTheme";
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 import React from "react";
 export default function TabsLayout() {
+  const { colors } = useTheme();
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "red",
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.textMuted,
         tabBarStyle: {
-            backgroundColor: "#1e293b",
+          backgroundColor: colors.surface,
           borderTopWidth: 1,
+          borderTopColor: colors.border,
           paddingTop: 10,
         },
-        tabBarLabelStyle:{
-            fontSize: 9,
+        tabBarLabelStyle: {
+          fontSize: 9,
         },
         headerShown: false,
       }}
