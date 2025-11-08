d7ec04db72c5f8c3880a15dd091538d892e0ebc7
Akashonbeats <akash.s1204@gmail.com>
2025-11-08 20:54:50 +0530

    Added Header and TodoInput

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import { StatusBar, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {LinearGradient} from "expo-linear-gradient";
 import Header from "@/components/Header";
+import TodoInput from "@/components/TodoInput";
 
 export default function Index() {
   const { toggleDarkMode, colors } = useTheme(); //Importing Colors from mmy custom hook
@@ -13,6 +14,7 @@ export default function Index() {
       <StatusBar barStyle={colors.statusBarStyle}/>
       <SafeAreaView style={homeStyles.safeArea}>
         <Header/>
+        <TodoInput/>
         <TouchableOpacity onPress={toggleDarkMode}>
           <Text>Switch to Dark Mode</Text>
         </TouchableOpacity>
